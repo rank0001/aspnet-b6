e89209bb554cac830942cfd65a49c71634eea638
zubayer <zubayerahmed232@gmail.com>
2022-03-19 21:39:01 +0600

    task 1 done

--- Exam1/Task1/Program.cs
+++ Exam1/Task1/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-
+            List<int> lists = new List<int> { 1, 2, 3 };
+            Func<int, int, int> output = (x,y)=> (x*y);
+            var value = SmartSort.Sort<int>(lists, output);
         }
+        
+
+        
+    }
+    public static class SmartSort { 
+        public static IList<T> Sort<T>(this IList<T> elements, Func<T, T, int> compare) 
+        {
+            throw new NotImplementedException(); 
+        } 
     }
 }
