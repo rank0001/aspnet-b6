455e86cfd6535f761de0156b130e5e2457439c00
rank0001 <zubayerahmed232@gmail.com>
2022-06-07 11:54:18 +0600

    project name changed

--- AWS Operation/Bucket Task/Program.cs
+++ AWS Operation/Bucket Task/Program.cs
@@ -19,16 +19,16 @@ string filepath3 = @"C:\Users\test.txt";
 string downloadLocation = @"D:\dev skill materials\planProjectDownload.txt";
 
 #region UploadFile
-//await bucketOperations.UplodFileAsync(bucketName, fileName3, filepath3);
+await bucketOperations.UplodFileAsync(bucketName, fileName3, filepath3);
 
 #endregion
 
 #region Download File
-//bucketOperations.DownloadObject(bucketName, fileName1, @"D:\dev skill materials\planProjectDownload.txt");
+bucketOperations.DownloadObject(bucketName, fileName1, @"D:\dev skill materials\planProjectDownload.txt");
 #endregion
 
 #region Deleting a file from bucket 
-//bucketOperations.DeleteObject(bucketName, fileName1);
+bucketOperations.DeleteObject(bucketName, fileName1);
 #endregion
 
 
--- AWS Operation/SQS Operation/Program.cs
+++ AWS Operation/SQS Operation/Program.cs
@@ -12,28 +12,28 @@ var myUrl = "https://sqs.us-east-1.amazonaws.com/847888492411/zubayerSQS";
 #endregion
 
 #region Adding Message to a queue
-//int j = 0;
-//do
-//{
-//    var messageId = await queueOperation.AddMessage(myUrl, body);
-//    j++;
-//    Console.WriteLine(messageId);
-//} while (j != 10);
+int j = 0;
+do
+{
+    var messageId = await queueOperation.AddMessage(myUrl, body);
+    j++;
+    Console.WriteLine(messageId);
+} while (j != 10);
 #endregion
 
 #region For reading  messages in queue;
-//int numbers = 10;
-//var messages = await queueOperation.ReadMessageAsync(myUrl, numbers);
+int numbers = 10;
+var messages = await queueOperation.ReadMessageAsync(myUrl, numbers);
 
-//foreach (var message in messages)
-//{
-//    Console.WriteLine(message);
-//}
+foreach (var message in messages)
+{
+    Console.WriteLine(message);
+}
 #endregion
 
 #region for reading and deleting messages
-//int messageNumber = 5;
-//await queueOperation.ReceiveAndDeleteMessages(myUrl,messageNumber);
+int messageNumber = 5;
+await queueOperation.ReceiveAndDeleteMessages(myUrl, messageNumber);
 #endregion
 
 
